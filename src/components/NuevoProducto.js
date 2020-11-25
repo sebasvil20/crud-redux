4163735ee89f3c986e7b7755a7c425f81533b87c
Sebastian Villegas <sebasvil20@gmail.com>
2020-11-25 07:55:20 -0500

    Initializing app
    installing dependencies, initial design and routing

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,42 @@
+const NuevoProducto = () => {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form>
+              <div class="form-group">
+                <label>Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre producto"
+                  name="nombre"
+                />
+                <label>Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio producto"
+                  name="precio"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NuevoProducto;
